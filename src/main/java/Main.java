6de728729ad209d27935b1221e6c9d20d0f6ce1a
Vadym Tutetskyi <sequzee@gmail.com>
2026-09-21 19:18:38 -0400

    lastCommit?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,11 +19,11 @@ public class Main {
     }
     public static int[] bubbleSort(int[] array){//bubble sort
         for (int i = 0;i<array.length-1;i++){
-            for (int j = i+1;j<array.length;j++){
-                if (array[i]>array[j]){
-                    int temp = array[j];
-                    array[j] = array[i];
-                    array[i] = temp;
+            for (int j = 0;j<array.length-1;j++){
+                if (array[j]>array[j+1]){
+                    int temp = array[j+1];
+                    array[j+1] = array[j];
+                    array[j] = temp;
                 }
             }
         }
